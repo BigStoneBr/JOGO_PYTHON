13795dc6b8cea8ee86af7a332c23577259a76cb6
Daniel Rocha <danielrochaqueiroz055@gmail.com>
2026-05-20 23:27:06 -0300

    Colocando os textos no Menu

--- code/game.py
+++ code/game.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
 
@@ -21,8 +22,4 @@ class Game:
 
 
 
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # Close Window
-            #         quit()  # end pygame
+            
